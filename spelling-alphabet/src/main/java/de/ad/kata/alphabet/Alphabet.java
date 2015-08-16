0fcd4025baee19bc0c10b4d1c043f4111890e2be
a11n <andre.diermann@gmail.com>
2015-08-16 10:42:30 +0200

    Implemented code to make extended test pass.

--- spelling-alphabet/src/main/java/de/ad/kata/alphabet/Alphabet.java
+++ spelling-alphabet/src/main/java/de/ad/kata/alphabet/Alphabet.java
@@ -1,7 +1,19 @@
 package de.ad.kata.alphabet;
 
+import java.util.Arrays;
+
 public class Alphabet {
+  private static final String[] WORDS = { "alfa", "bravo" };
+
   public String spell(char character) {
-    return "alfa";
+    return lookUpWordFor(character);
+  }
+
+  private String lookUpWordFor(final char character) {
+    return Arrays.stream(WORDS)
+        .filter(word -> word.indexOf(character) == 0)
+        .findFirst()
+        .orElseThrow(() -> new IllegalArgumentException(
+            String.format("'%s' cannot be spelled.", character)));
   }
 }
