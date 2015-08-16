f904f815ac5936140c0eb389e0830de28cd33ac3
a11n <andre.diermann@gmail.com>
2015-08-16 22:17:45 +0200

    Implemented first test (failing).

--- roman-numerals/src/test/java/de/ad/kata/numerals/RomanNumeralTest.java
+++ roman-numerals/src/test/java/de/ad/kata/numerals/RomanNumeralTest.java
@@ -1,4 +1,13 @@
 package de.ad.kata.numerals;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class RomanNumeralTest {
+  @Test
+  public void testConversionToRomanNumeral() throws Exception {
+    assertThat(RomanNumeral.of(1), is("I"));
+  }
 }
