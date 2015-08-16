70b004be1d100a319faabb06259c7817730e5903
a11n <andre.diermann@gmail.com>
2015-08-16 11:01:39 +0200

    Extended test to test for full alphabet. Implemented code to make extended test pass.

--- spelling-alphabet/src/main/java/de/ad/kata/alphabet/Alphabet.java
+++ spelling-alphabet/src/main/java/de/ad/kata/alphabet/Alphabet.java
@@ -3,7 +3,10 @@ package de.ad.kata.alphabet;
 import java.util.Arrays;
 
 public class Alphabet {
-  private static final String[] WORDS = { "alfa", "bravo" };
+  private static final String[] WORDS =
+      { "Alfa", "Bravo", "Charlie", "Delta", "Foxtrot", "Echo", "Golf", "Hotel", "India", "Juliett",
+          "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango",
+          "Uniform", "Victor", "Whiskey", "X-Ray", "Yankee", "Zulu" };
 
   public String spell(char character) {
     return lookUpWordFor(character);
@@ -11,7 +14,7 @@ public class Alphabet {
 
   private String lookUpWordFor(final char character) {
     return Arrays.stream(WORDS)
-        .filter(word -> word.indexOf(character) == 0)
+        .filter(word -> word.indexOf(Character.toUpperCase(character)) == 0)
         .findFirst()
         .orElseThrow(() -> new IllegalArgumentException(
             String.format("'%s' cannot be spelled.", character)));
--- spelling-alphabet/src/test/java/de/ad/kata/alphabet/AlphabetTest.java
+++ spelling-alphabet/src/test/java/de/ad/kata/alphabet/AlphabetTest.java
@@ -17,7 +17,39 @@ public class AlphabetTest {
 
   @Test
   public void testCharacterSpelling() throws Exception {
-    assertThat(alphabet.spell('a'), is("alfa"));
-    assertThat(alphabet.spell('b'), is("bravo"));
+    assertThat(alphabet.spell('a'), is("Alfa"));
+    assertThat(alphabet.spell('b'), is("Bravo"));
+    assertThat(alphabet.spell('c'), is("Charlie"));
+    
+    assertThat(alphabet.spell('d'), is("Delta"));
+    assertThat(alphabet.spell('e'), is("Echo"));
+    assertThat(alphabet.spell('f'), is("Foxtrot"));
+    
+    assertThat(alphabet.spell('g'), is("Golf"));
+    assertThat(alphabet.spell('h'), is("Hotel"));
+    assertThat(alphabet.spell('i'), is("India"));
+    
+    assertThat(alphabet.spell('j'), is("Juliett"));
+    assertThat(alphabet.spell('k'), is("Kilo"));
+    assertThat(alphabet.spell('l'), is("Lima"));
+    
+    assertThat(alphabet.spell('m'), is("Mike"));
+    assertThat(alphabet.spell('n'), is("November"));
+    assertThat(alphabet.spell('o'), is("Oscar"));
+
+    assertThat(alphabet.spell('p'), is("Papa"));
+    assertThat(alphabet.spell('q'), is("Quebec"));
+    assertThat(alphabet.spell('r'), is("Romeo"));
+
+    assertThat(alphabet.spell('s'), is("Sierra"));
+    assertThat(alphabet.spell('t'), is("Tango"));
+    assertThat(alphabet.spell('u'), is("Uniform"));
+
+    assertThat(alphabet.spell('v'), is("Victor"));
+    assertThat(alphabet.spell('w'), is("Whiskey"));
+    assertThat(alphabet.spell('x'), is("X-Ray"));
+
+    assertThat(alphabet.spell('y'), is("Yankee"));
+    assertThat(alphabet.spell('z'), is("Zulu"));
   }
 }
