d31bb333c0860d20a1aea6288c2e82b4c34bfd5d
a11n <andre.diermann@gmail.com>
2015-08-16 22:39:41 +0200

    Extended test to test more functionality.

--- roman-numerals/src/test/java/de/ad/kata/numerals/RomanNumeralTest.java
+++ roman-numerals/src/test/java/de/ad/kata/numerals/RomanNumeralTest.java
@@ -9,5 +9,15 @@ public class RomanNumeralTest {
   @Test
   public void testConversionToRomanNumeral() throws Exception {
     assertThat(RomanNumeral.of(1).toString(), is("I"));
+    assertThat(RomanNumeral.of(2).toString(), is("II"));
+    assertThat(RomanNumeral.of(3).toString(), is("III"));
+    
+    assertThat(RomanNumeral.of(4).toString(), is("IV"));
+    assertThat(RomanNumeral.of(5).toString(), is("V"));
+    assertThat(RomanNumeral.of(6).toString(), is("VI"));
+    
+    assertThat(RomanNumeral.of(9).toString(), is("IX"));
+    assertThat(RomanNumeral.of(10).toString(), is("X"));
+    assertThat(RomanNumeral.of(11).toString(), is("XI"));
   }
 }
