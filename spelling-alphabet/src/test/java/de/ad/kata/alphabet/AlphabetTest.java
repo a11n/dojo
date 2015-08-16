49def3839863e02f175844464c1f8a12637a40fa
a11n <andre.diermann@gmail.com>
2015-08-16 10:18:02 +0200

    Implemented first test.

--- spelling-alphabet/src/test/java/de/ad/kata/alphabet/AlphabetTest.java
+++ spelling-alphabet/src/test/java/de/ad/kata/alphabet/AlphabetTest.java
@@ -1,4 +1,24 @@
 package de.ad.kata.alphabet;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class AlphabetTest {
+
+  private Alphabet alphabet;
+
+  @Before
+  public void setUp() throws Exception {
+    alphabet = new Alphabet();
+  }
+
+  @Test
+  public void testCharacterSpelling() throws Exception {
+    String actual = alphabet.spell('a');
+
+    assertThat(actual, is("alfa"));
+  }
 }
