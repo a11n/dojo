5e4bd9b5ffcc7fd684856184f1349581dc8a79b1
a11n <andre.diermann@gmail.com>
2015-08-17 16:22:39 +0200

    Implemented code to make extended test pass.

--- roman-numerals/src/main/java/de/ad/kata/numerals/RomanNumeral.java
+++ roman-numerals/src/main/java/de/ad/kata/numerals/RomanNumeral.java
@@ -1,17 +1,28 @@
 package de.ad.kata.numerals;
 
 public class RomanNumeral {
+  private final static int[] VALUES = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+  private final static String[] SYMBOLS =
+      { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
   private final int number;
 
-  private RomanNumeral(int number){
+  private RomanNumeral(int number) {
     this.number = number;
   }
-  
+
   public static RomanNumeral of(int number) {
     return new RomanNumeral(number);
   }
 
   @Override public String toString() {
-    return "I";
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0, rest = number; i < VALUES.length; i++) {
+      for (; rest / VALUES[i] > 0; rest -= VALUES[i]) {
+        builder.append(SYMBOLS[i]);
+      }
+    }
+
+    return builder.toString();
   }
 }
